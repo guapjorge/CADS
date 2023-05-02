f00314b128cb7f1aff91936a11a3148d0729d64b
Aaron DeLara <aarondelara64@gmail.com>
2023-05-01 21:30:38 -0700

    added youtube api key and enter button to search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-// pls work
+var inputUser = document.querySelector('#input-username')
+var searchButton = document.querySelector('#search-button')
+function findUserName(id) {
+    fetch('https://www.googleapis.com/youtube/v3/channels&appid=AIzaSyAIkxCpJ31trsgp_oqRjz9IflOIUIOjiUw')
+    
+}
+
+searchButton.addEventListener('click', function () {
+    var searchedUser = inputUser.value
+    findUserName(searchedUser)
+})
+
+inputUser.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.querySelector('#search-button').click();
+    }
+})
+
+function 
+    var url = 
+    url: `https://check-username.p.rapidapi.com/check/${url}/${username}`;
