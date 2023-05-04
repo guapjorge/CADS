0b1709c7ef667b15bb69fe72e5d0831df1f9b2d9
Aaron DeLara <aarondelara64@gmail.com>
2023-05-03 21:42:23 -0700

    added youtube sample and uncommented out some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,12 +25,12 @@ inputUser.addEventListener('keypress', function (event) {
     }
 })
 
-/* function 
+function 
     var url = 
     url: `https://check-username.p.rapidapi.com/check/${url}/${username}`;
 
     function checksocial(username) {
-        // console.log("HELLO WORLD!")
+        console.log("HELLO WORLD!")
         fetch(https://    .then(function (res) {
             return res.json()
           })
@@ -40,30 +40,28 @@ inputUser.addEventListener('keypress', function (event) {
       }
       
       checksocial("blah")
-      */
 
 function authenticate() {
     return gapi.auth2.getAuthInstance()
-        .signIn({
-            scope: "https://www.googleapis.com/auth/youtube.readonly)"
-                .then(function () { console.log("Sign-in successful"); },
-                    function (err) { console.error("Error signing in", err); })
-        })
+        .signIn({ scope: "https://www.googleapis.com/auth/youtube.readonly" })
+        .then(function () { console.log("Sign-in successful"); },
+            function (err) { console.error("Error signing in", err); });
 }
 
 function loadClient() {
     gapi.client.setApiKey("AIzaSyAIkxCpJ31trsgp_oqRjz9IflOIUIOjiUw");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest%22")
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function () { console.log("GAPI client loaded for API"); },
             function (err) { console.error("Error loading GAPI client for API", err); });
 }
+
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute() {
     return gapi.client.youtube.channels.list({
         "part": [
-            "snippet,contentDetails,statistics"
+            "brandingSettings"
         ],
-        "forUsername": "USERNAME_HERE"
+        "mine": true
     })
         .then(function (response) {
             // Handle the results here (response.result has the parsed body).
@@ -71,6 +69,15 @@ function execute() {
         },
             function (err) { console.error("Execute error", err); });
 }
+
 gapi.load("client:auth2", function () {
     gapi.auth2.init({ client_id: "YOUR_CLIENT_ID" });
-});
+})
+
+fetch(
+    "https://checkmarks.com/api/v1/username/andresray/account/ARS2989/password/cQgPy9VnDN"
+  )
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
